e7d92e216d882e63195dd548f6d3e06be0de2f72
Alini Ribeiro <aliniribeiroo@gmail.com>
2026-03-14 13:13:57 -0300

    fix: add create conditionals

--- modules/efs_filesystem/outputs.tf
+++ modules/efs_filesystem/outputs.tf
@@ -4,33 +4,33 @@
 
 output "creation_token" {
   description = "EFS creation token"
-  value       = aws_efs_file_system.this.creation_token
+  value       = try(aws_efs_file_system.this[0].creation_token, null)
 }
 
 output "tags" {
   description = "EFS tags"
-  value       = aws_efs_file_system.this.tags
+  value       = try(aws_efs_file_system.this[0].tags, null)
 }
 
 output "throughput_mode" {
   description = "EFS throughput mode"
-  value       = aws_efs_file_system.this.throughput_mode
+  value       = try(aws_efs_file_system.this[0].throughput_mode, null)
 }
 
 output "performance_mode" {
   description = "EFS performance mode"
-  value       = aws_efs_file_system.this.performance_mode
+  value       = try(aws_efs_file_system.this[0].performance_mode, null)
 }
 
 output "file_system_id" {
   description = "EFS file system id"
-  value       = aws_efs_file_system.this.id
+  value       = try(aws_efs_file_system.this[0].id, null)
 }
 
 output "file_system_arn" {
-  value = aws_efs_file_system.this.arn
+  value = try(aws_efs_file_system.this[0].arn, null)
 }
 
 output "id" {
-  value = aws_efs_file_system.this.id
+  value = try(aws_efs_file_system.this.id, null)
 }
